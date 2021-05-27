64c5dcebec932af08eee71b9923c6aec180f87f6
qaz <1047963985@qq.com>
2021-05-27 15:07:06 +0800

    qaz

--- code/update.py
+++ code/update.py
@@ -9,6 +9,6 @@ os.chdir(sys.path[0])
 
 conn = sqlite3.connect(sys.path[0]+'/../data/shows.db')
 curs = conn.cursor()
-SQL = "CREATE TABLE if not exists forecast(
+#SQL = "CREATE TABLE if not exists forecast(
 conn.close()
 
